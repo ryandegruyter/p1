e0ce6294636ae974820c53477fddc0ebeafe4e07
Ryan <mandroid.be@gmail.com>
2015-08-27 22:08:36 +0200

    initial commit

---
+++ app/src/main/java/be/ryan/popularmovies/tmdb/TmdbWebServiceContract.java
@@ -0,0 +1,10 @@
+package be.ryan.popularmovies.tmdb;
+
+import be.ryan.popularmovies.BuildConfig;
+
+/**
+ * Created by Ryan on 27/08/2015.
+ */
+public class TmdbWebServiceContract {
+    static String API_KEY = BuildConfig.TMDB_API_KEY;
+}
